39f522dbdb37f3cb6c0283a059344eb4c012bb85
Kevin Choi <kevinnwjchoi@gmail.com>
2024-07-29 13:14:51 -0400

    created Home and added index.css

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container, Typography } from '@mui/material';
+import '../index.css'; 
+
+const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <Container sx={{ bgcolor: 'background.default', minHeight: '100vh', pt: 2 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Welcome, {currentUser ? currentUser.username : 'Guest'}
+      </Typography>
+      <Typography variant="body1" paragraph>
+        {/* Add more content here if needed */}
+      </Typography>
+    </Container>
+  );
+};
+
+export default Home;
